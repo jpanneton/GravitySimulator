1b9637e8003d2d9ced007b3ebc4f8cc0c9778b71
Jérémi Panneton <jeremi.panneton@outlook.com>
2020-01-03 23:04:25 +0100

    Measured performances

---
+++ GravitySimulator/Engine/Core/Time.h
@@ -0,0 +1,30 @@
+#include <chrono>
+#include <type_traits>
+#include <utility>
+
+namespace Time
+{
+    inline auto clockNow()
+    {
+        return std::chrono::high_resolution_clock::now();
+    }
+
+    template<class DurationType, class F, class ... Args>
+    auto measureExecutionTime(F f, Args&&... args)
+    {
+        if constexpr (std::is_void_v<std::invoke_result_t<F, Args...>>)
+        {
+            const auto start = clockNow();
+            f(std::forward<Args>(args)...);
+            const auto end = clockNow();
+            return std::chrono::duration_cast<DurationType>(end - start).count();
+        }
+        else
+        {
+            const auto start = clockNow();
+            const auto result = f(std::forward<Args>(args)...);
+            const auto end = clockNow();
+            return std::make_pair(result, std::chrono::duration_cast<DurationType>(end - avant).count());
+        }
+    }
+}
